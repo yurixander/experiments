605eab101bc875cd65dc57088e978561a7a4f264
yurixander <101931215+yurixander@users.noreply.github.com>
2023-12-09 22:54:27 -0500

    Draw grid

--- 3d-rotation/app.js
+++ 3d-rotation/app.js
@@ -8,14 +8,36 @@ function assert(condition, reasoning) {
   }
 }
 
-window.addEventListener("load", () => {
-  console.log("Logic loaded")
-
+function render(vertices) {
   const $canvas = document.getElementById("canvas")
 
-  assert($canvas !== null, "canvas element should exist")
+  $canvas.innerHTML = ""
 
-  const context = $canvas.getContext("2d")
+  for (const vertex of vertices) {
+    const $vertex = document.createElement("div")
 
-  console.log(context)
+    $vertex.className = "vertex"
+    $vertex.style.left = `${vertex.x}px`
+    $vertex.style.top = `${vertex.y}px`
+    $canvas.appendChild($vertex)
+  }
+}
+
+window.addEventListener("load", () => {
+  console.log("logic loaded")
+
+  const vertices = []
+  const WIDTH = 60
+  const HEIGHT = 30
+  const GAP = 20
+
+  for (let x = 0; x < WIDTH; x++)
+    for (let y = 0; y < HEIGHT; y++)
+      vertices.push({
+        x: x * GAP,
+        y: y * GAP,
+        z: 0
+      })
+
+  render(vertices)
 })
